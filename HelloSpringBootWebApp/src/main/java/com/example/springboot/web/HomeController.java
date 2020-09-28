0240b648c7d92a28c41b8d3a7f2cd79effd4a3a1
Mohaiminul Islam <63113749+Mohaiminul036@users.noreply.github.com>
2020-09-28 22:19:05 +0600

    Add files via upload

---
+++ HelloSpringBootWebApp/src/main/java/com/example/springboot/web/HomeController.java
@@ -0,0 +1,30 @@
+package com.example.springboot.web;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/")
+
+public class HomeController {
+	private static final SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+    
+	@GetMapping(value = "/")
+    //@RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView welcome() {
+        final String VIEW_INDEX = "index";
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("message", "Welcome!");
+        mv.addObject("date", SDF.format(new Date()));
+        mv.setViewName(VIEW_INDEX);
+
+        return mv;
+    }
+
+}
